a8b6e3e2690c974297b2448569445e62a653a71f
gaelmos <156100424+gaelmos@users.noreply.github.com>
2024-06-26 14:53:52 -0300

    parte casi echa

--- Assets/juegos.cs
+++ Assets/juegos.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class juegos : MonoBehaviour
 {
+    int sumatotal;
+
+
     public int cantidaddinero;
     public GameObject[] objetos;
     public Transform lugar1;
@@ -19,22 +22,30 @@ public class juegos : MonoBehaviour
     public Button falta;
     public Button jugarotravez;
     public Button salir;
-
-    private producto objeto1;
-    private producto objeto2;
+    
+    private int objeto1;
+    private int objeto2;
     private GameObject ponerob1;
     private GameObject ponerob2;
     // Start is called before the first frame update
     void Start()
     {
+     
         GenerarProductos();
         panel.SetActive(false);
+        GenerarCantidadDinero();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void GenerarCantidadDinero()
+    {
+        
+        cantidaddinero = Random.Range(50, 1000);
+        dineroactual.text = "Dinero: $" + cantidaddinero.ToString();
     }
     void GenerarProductos()
     {
@@ -44,11 +55,57 @@ public class juegos : MonoBehaviour
         ponerob1 = Instantiate(objetos[Random.Range(0, objetos.Length)], lugar1.position, Quaternion.identity);
         ponerob2 = Instantiate(objetos[Random.Range(0, objetos.Length)], lugar2.position, Quaternion.identity);
 
-        objeto1 = ponerob1.GetComponent<producto>();
-        objeto2 = ponerob2.GetComponent<producto>();
+        Producto producto1 = ponerob1.GetComponent<Producto>();
+        Producto producto2  = ponerob2.GetComponent<Producto>();
+        sumatotal = producto1.precio + producto2.precio;
+        precios1.text = objeto1.ToString();
+        precios2.text =  objeto2.ToString();
+    }
+    void VerificarAlcanzaYSobra()
+    {
+        
+        bool bien = cantidaddinero > sumatotal;
+        MostrarResultado(bien);
+    }
 
-        precios1.text = "$" + objeto1.ToString();
-        precios2.text = "$" + objeto2.ToString();
-        dineroactual.text = "Dinero: $" + dineroactual.ToString();
+    void VerificarAlcanzaJusto()
+    {
+        ;
+        bool bien = cantidaddinero == sumatotal;
+        MostrarResultado(bien);
+    }
+
+    void VerificarNoAlcanza()
+    {
+        bool bien = cantidaddinero < sumatotal;
+        MostrarResultado(bien);
+    }
+
+    void MostrarResultado(bool bien)
+    {
+        panel.SetActive(true);
+        if (bien)
+        {
+            resultado.text = "¡Correcto!";
+            jugarotravez.GetComponentInChildren<Text>().text = "Reiniciar el desafío";
+        }
+        else
+        {
+            resultado.text = "Incorrecto.";
+            jugarotravez.GetComponentInChildren<Text>().text = "Volver a intentarlo";
+        }
+    }
+
+    public void JugarOtraVez()
+    {
+        panel.SetActive(false);
+        GenerarProductos();
+    }
+
+    public void Salir()
+    {
+        // Aquí cargas la escena de selección de juegos
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SeleccionarJuegos");
     }
 }
+
